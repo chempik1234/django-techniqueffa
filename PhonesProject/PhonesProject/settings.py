ef266fec8140115d535e1b49c53f2787357743fd
Danis <dandijar@yandex.ru>
2024-05-18 23:35:30 +0500

    vercel config

--- PhonesProject/PhonesProject/settings.py
+++ PhonesProject/PhonesProject/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'  # os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ PhonesProject/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Update pip
+echo "Updating pip..."
+python3.12 pip install -U pip
+
+# Install dependencies
+
+echo "Installing project dependencies..."
+python3.12 -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+
+# Collect staticfiles
+echo "Collect static..."
+python3.12 manage.py collectstatic --noinput --clear
+
+echo "Build process completed!"
