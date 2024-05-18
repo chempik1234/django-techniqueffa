1007c4060ee5f36a807764e47eb5becfc6720a57
Danis <dandijar@yandex.ru>
2024-05-18 23:10:01 +0500

    ready for vercel?

--- PhonesProject/PhonesProject/settings.py
+++ PhonesProject/PhonesProject/settings.py
@@ -83,10 +83,20 @@ WSGI_APPLICATION = 'PhonesProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DATABASE"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': '5432',
     }
 }
 
