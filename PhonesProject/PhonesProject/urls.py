a39c2b1a21318948591f77d5f972e326ab8c920f
Danis <dandijar@yandex.ru>
2024-05-19 02:31:13 +0500

    heavy artillery x2

--- PhonesProject/PhonesProject/urls.py
+++ PhonesProject/PhonesProject/urls.py
@@ -29,10 +29,10 @@ urlpatterns = [
     path('', include('static_pages.urls', namespace='static_pages')),
 ]
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
-]
+# urlpatterns += [
+#     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+#     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+# ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
