99a3a6bcf23779da10b6c95d480f6b0eebbf15a3
Danis <dandijar@yandex.ru>
2024-05-19 01:34:42 +0500

    test

--- PhonesProject/build.sh
+++ PhonesProject/build.sh
@@ -2,5 +2,5 @@ pip install -r requirements.txt
 
 # make migrations
 python3.9 manage.py migrate
-python3.9 manage.py findstatic staticfile close_button.js
+python3.9 manage.py findstatic js/close_button.js
 python3.9 manage.py collectstatic
