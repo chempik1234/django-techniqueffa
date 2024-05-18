8a11b44e46e4b0b08d392695e609015e41143aee
Danis <dandijar@yandex.ru>
2024-05-19 00:08:10 +0500

    oof

--- PhonesProject/build.sh
+++ PhonesProject/build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+# Install pip first
+# echo "Installing pip..."
+# sudo apt install python3-pip
+
 # Update pip
 echo "Updating pip..."
-python3.9 pip install -U pip
+python3.12 pip install -U pip
 
 # Install dependencies
 
 echo "Installing project dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 # Collect staticfiles
 echo "Collect static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"
