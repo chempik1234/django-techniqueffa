ef87b95d49b779926520f681b34261bfbe1a6854
Danis <dandijar@yandex.ru>
2024-05-19 00:36:02 +0500

    static

--- PhonesProject/PhonesProject/settings.py
+++ PhonesProject/PhonesProject/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
 STATIC_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
