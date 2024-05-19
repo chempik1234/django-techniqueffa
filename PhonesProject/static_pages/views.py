c4e9b60c4c6f7e876984a31d859b94b32e94a970
Danis <dandijar@yandex.ru>
2024-05-19 11:36:12 +0500

    static pages no default context

--- PhonesProject/static_pages/views.py
+++ PhonesProject/static_pages/views.py
@@ -1,20 +1,16 @@
-from django.conf import settings
 from django.shortcuts import render
 
 
 def contacts_page(request):
-    data = settings.DEFAULT_CONTEXT.copy()
-    data["title_header"] = "Контакты"
+    data = {"title_header": "Контакты"}
     return render(request, 'static_pages/contacts.html', context=data)
 
 
 def support_page(request):
-    data = settings.DEFAULT_CONTEXT.copy()
-    data["title_header"] = "Поддержка и помощь"
+    data = {"title_header": "Поддержка и помощь"}
     return render(request, "static_pages/support.html", context=data)
 
 
 def about_page(request):
-    data = settings.DEFAULT_CONTEXT.copy()
-    data["title_header"] = "О магазине"
+    data = {"title_header": "О магазине"}
     return render(request, "static_pages/about.html", context=data)
