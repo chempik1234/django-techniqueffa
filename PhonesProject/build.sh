7e045db2b1ae86e342c1c7cb5920d0afc0933959
Danis <dandijar@yandex.ru>
2024-05-19 14:20:06 +0500

    cloudinary! 2

--- PhonesProject/build.sh
+++ PhonesProject/build.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-python3.9 manage.py findstatic js/close_button.js
-python3.9 manage.py collectstatic
+# python3.9 manage.py findstatic js/close_button.js
+# python3.9 manage.py collectstatic
